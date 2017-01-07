0c69f6b6f94f04a012063d2e2db7fa225fa22fd8
Alex Fang <frjalex@gmail.com>
2017-01-07 01:34:54 -0500

    初始登记

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    struct node * next;
+} node ;
+
+int main() {
+    return 0;
+}
---
+++ yii.h
@@ -0,0 +1,80 @@
+//
+// Created by alex on 1/6/17.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#define 如果 if
+#define 其他 else
+#define 空值 void
+#define 短数字 short
+#define 数字 int
+#define 长值 long
+#define 单精度浮点数 float
+#define 双精度浮点数 double
+#define 类定义 typedef
+#define 结构体 struct
+#define 返回 return
+#define 指针 *
+#define 命令行显示 printf
+#define 物体大小 sizeof
+#define 循环 while
+#define 做 {
+#define 完成 }
+#define 开始 {
+
+#define 结束 }
+
+#define 行末 ;
+
+#define 入口函数 main
+
+#define 命令数量 argc
+
+#define 命令数组 argv
+
+#define 马洛个 malloc
+
+#define 重新马洛个 realloc
+
+#define 扔掉 free
+
+#define 加 +
+#define 减 -
+#define 乘 *
+#define 除 /
+#define 大于 >
+#define 小于 <
+#define 赋值 =
+#define 等于 ==
+#define 不 !
+#define 加一 ++
+#define 减一 --
+#define 自加 +=
+#define 自减 -=
+#define 自乘 *=
+#define 自除 /=
+#define 不是 !=
+#define 左起 (
+#define 右放 )
+
+#define 一 1
+#define 二 2
+#define 三 3
+#define 四 4
+#define 五 5
+#define 六 6
+#define 七 7
+#define 八 8
+#define 九 9
+#define 十 10
+#define 零 0
+#define 百 100
+#define 千 1000
+#define 万 10000
+#define 十万 100000
+#define 百万 1000000
+#define 千万 10000000
+#define 亿 10000000
